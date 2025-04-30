0ba37690bb5ba20af1b1408fc49599c44a52eadb
GiorgioMaPa2 <giorgiopm03@gmail.com>
2025-04-30 09:57:53 +0200

    Provato a parallelizzare la prima parte. Il nuovo file di riferimento e' il 5

--- out_check.sh
+++ out_check.sh
@@ -4,8 +4,8 @@
 # cambia il nome dei file per fare test su file diversi
 
 # Confronta i due file riga per riga
-diff omp_test_files/comp_seq100D2.inp out_files/mpi_100D2.inp > /dev/null
-echo "test MPI 100D2"
+diff omp_test_files/test7.inp omp_test_files/test5.inp > /dev/null
+#echo "test MPI 100D2"
 # Verifica l'esito del comando diff
 if [ $? -eq 0 ]; then
     echo "I file sono uguali"
